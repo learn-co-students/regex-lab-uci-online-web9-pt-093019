8ee2b6f182a01c284c06279725c62824d1a0e3cd
Andrew Capp <acapp909@gmail.com>
2019-10-25 03:22:38 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,31 +1,26 @@
 require 'pry'
- 
-def starts_with_a_vowel?(word)
- word.match(/\A[aeiouAEIOU]\w+/) ? true : false
+def starts_with_a_vowel?(string)
+  string.match?(/\A[a,e,i,o,u,A,E,I,O,U]/)
 end
 
-def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/un\w+ing/)
-end
+def words_starting_with_un_and_ending_with_ing(string)
+  string.scan(/un\w+ing/)
 
-def words_five_letters_long(text)
-  text.scan(/\b\w{5}\b/)
-end
-
-def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/^[A-Z].*\.$/) != nil 
-    return true
-  else
-    return false
-end
 
 end
 
-def valid_phone_number?(phone)
-  if phone.match(/[0-9][0-9][0-9].?[0-9][0-9][0-9].?[0-9][0-9][0-9][0-9]/) != nil 
-    return true 
-  else
-    return false 
-  end
-  
+def words_five_letters_long(string)
+
+  string.scan(/\b\w{5}\b/)
+
+end
+
+def first_word_capitalized_and_ends_with_punctuation?(string)
+  string.match?(/\A[A-Z].+[.,!,?]$/)
+  #binding.pry
+end
+
+def valid_phone_number?(number)
+  number.match(/[0-9][0-9][0-9].?[0-9][0-9][0-9].?[0-9][0-9][0-9][0-9]/)
+
 end
